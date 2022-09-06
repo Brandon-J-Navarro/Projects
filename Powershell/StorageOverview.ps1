9f58ba5c4945ccdbd3070574ca6234ae7ee524a6
Brandon <Brandon-N01@users.noreply.github.com>
2022-09-06 12:08:41 -0400

    Create StorageOverview.ps1

---
+++ Powershell/StorageOverview.ps1
@@ -0,0 +1,8 @@
+$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText
+Get-PSDrive -name <#driveleter#> | 
+    Select-Object @{name="File Path";Expression="Root"},
+    @{name='Used (GB)';expression={($_.used/1gb).tostring("#.##")}},
+    @{name='Free (GB)';expression={($_.free/1gb).tostring("#.##")}},
+    @{name='Size (GB)';expression={(($_.used+$_.free)/1gb).ToString('#.##')}} |
+    Format-list |
+    Out-File <#file\path.txt#>
