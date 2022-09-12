025bc170f907121d2348de022e8a6f639df63988
Brandon <brandon.j.navarro@outlook.com>
2022-09-11 21:52:59 -0400

    Update RemoteShutdown.ps1
    Version 2.0

--- Powershell/RemoteShutdown.ps1
+++ Powershell/RemoteShutdown.ps1
@@ -1,33 +1,52 @@
 <#
-REMOTE SHUTDOWN SCRIPT
-BLOCK 1
-THIS CREATES THE ALIASES FOR THE CREDENTIALS TO CONNECT TO REMOTE COMPUTER
+
+Remote shutdown script
+Version 2.0
+
+This new version uses RSA SSh Keys to connect to remote hosts
+
+Once the uninterruptible power supply (UPS) starts the shutdown process, 
+Task Scheduler will start this script based off the event ID that the UPS generates
+
 #>
-$User = "*COMPUTERNAME\USERNAME*"
-$PWord = ConvertTo-SecureString -String "*PASSWORD*" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-<#
-BLOCK 2
-THIS SENDS COMMANDS TO REMOTE COMPUTER OR COMPUTERS AS A JOB
-AND PROVIEDS THE CREDENTIALS WE SET UP IN BLOCK 1
-#>
-Invoke-Command -ComputerName '*COMPUTERNAME*' -AsJob `
--JobName Remoteconnect -Credential $Credential -ScriptBlock {
-    Get-ComputerInfo -Property csname,oslocaldatetime
-   #Stop-Computer -Force
-}
-<#
-BLOCK 3
-THIS WILL MAKE POWERSHELL PAUSE BEFORE CONTINUEING
-#>
-start-sleep -Seconds 5
-<#
-BLOCK 4
-THIS WILL FORMAT RESULTS AS PLAIN TEXT AND NOT HAVE EXTRA CHARACTERS AROUND TABLE NAMES
-AND GET THE RESULTS OF THE JOB CREATED IN BLOCK 2 AND OUT PUT IT TO A FILE
-#>
-$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText
-Receive-Job -name Remoteconnect |
-    Format-Table @{name='ComputerName';expression={$_.csname}},
-    @{name='RestartTime';expression={$_.oslocaldatetime}} |
-    out-file <#FILEPATH.txt#>
+$linux = New-PSSession -HostName <#IP ADDRESS#> -UserName <#USERNAME#> -KeyFilePath 'C:\FILE\PATH\TO\RSA\FILE'
+
+$hyperv = New-PSSession -HostName <#IP ADDRESS#> -UserName <#USERNAME#> -KeyFilePath 'C:\FILE\PATH\TO\RSA\FILE'
+
+$nas = New-PSSession -HostName <#IP ADDRESS#> -UserName <#USERNAME#> -KeyFilePath 'C:\FILE\PATH\TO\RSA\FILE'
+
+Invoke-Command -Session $linux -AsJob -JobName linux -ScriptBlock {
+    Get-Date
+    hostname
+    docker ps -a --format "table {{.Names}}"
+    docker container stop $(docker container list -q)
+    Start-Sleep 60s
+    docker ps -a --format "table {{.Names}}"
+    Stop-Computer
+    }
+
+Invoke-Command -Session $hyperv -AsJob -JobName hyperv -ScriptBlock {
+    Get-Date
+    hostname
+    Get-VM | Where-Object State -eq running | Stop-VM
+    Start-Sleep 60s
+    Get-VM | Where-Object State -eq running | Select-Object vmname
+    Stop-Computer
+    }
+
+Invoke-Command -Session $nas -AsJob -JobName nas -ScriptBlock {
+    Get-Date
+    hostname
+    Get-PSDrive -name C,S | 
+    Select-Object @{name="File Path";Expression="Root"},
+    @{name='Used (GB)';expression={($_.used/1gb).tostring("#.##")}},
+    @{name='Free (GB)';expression={($_.free/1gb).tostring("#.##")}},
+    @{name='Size (GB)';expression={(($_.used+$_.free)/1gb).ToString('#.##')}} |
+    Format-list 
+    Stop-Computer
+    }
+
+
+Start-Sleep 60s
+
+Receive-Job -name linux,hyperv,nas | Out-File C:\FILE\PATH\TO\OUTPUT\FILE.txt
