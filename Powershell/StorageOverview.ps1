d4b898414b1da68e1e92870c1f7eb4551f15de10
Brandon <brandon.j.navarro@outlook.com>
2022-09-12 10:02:57 -0400

    Update StorageOverview.ps1

--- Powershell/StorageOverview.ps1
+++ Powershell/StorageOverview.ps1
@@ -1,3 +1,6 @@
+<#
+This script is to get an over view of storage drives to be inserted into BGInfo
+#>
 Get-PSDrive -name <#drive,letters#> | 
     Select-Object @{name="File Path";Expression="Root"},
     @{name='Used (GB)';expression={($_.used/1gb).tostring("#.##")}},
